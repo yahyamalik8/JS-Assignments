04db2c73afdf840d20f79c15ced63888e2d947bc
= <=>
2024-09-16 16:29:10 +0500

    command added

---
+++ chap 5/app.js
@@ -0,0 +1,160 @@
+console.log("hello world")
+
+
+
+
+
+
+// chapter 5
+
+//question  1
+
+
+
+
+// var num1 = +prompt('write any number ')
+// var num2 = +prompt('write any number ')
+// let symbol =num1+num2 
+// alert("the sum of "+num1+"and  " +num2+ "is "+  symbol)
+
+
+
+
+// question 2 chapter5
+
+
+
+
+
+// document.write("value after decleartion is undefined")
+
+// var value=+prompt("put a number")
+// document.write("<br><br>intial value is "+ value);
+// var add=+prompt(" add a  number")
+// let symbol =value+add 
+// document.write("<br>after addition value will be  value is "+ symbol);
+
+
+
+// qestion 4 chapter 5
+
+// let ticket =+prompt("how many ticket do you want")
+// let eachTicket= 600
+
+// let final = ticket*eachTicket
+// document.write('You Book A  '   +  ticket+"     Ticket And Your Total is "+final )
+
+
+
+
+
+// qeustion 5 chapter 5
+
+
+// var value =+prompt ("write any number ")
+// for (let i = 1; i <=10; i++) {
+//     let final = value + " * "+ i +" = " +value*i
+//     console.log(final)
+
+
+
+    
+// }
+
+
+
+
+// question 6 chapter 5
+
+
+
+// let c =+prompt('write a number which do you want to change in celcius')
+// let f =+prompt('write a number which do you want to change in fahrenheit')
+
+
+// let final = (f-32)*5/9
+// let again = (c*9/5)+32
+
+// let digit=final.toFixed(2)
+// console.log(digit + "  c")
+
+// console.log(again + "  F")
+
+
+
+
+
+
+// question 7 chapter 5
+
+// let item2 =prompt('whats you want to purchase  on  my shoop')
+// let pant =+prompt('quantity of item');
+// let item1 =prompt('whats you want to purchase  on  my shoop')
+// let tShirt =+prompt('quantity of item');
+
+
+
+// document.write(" price of "+ item2 +"  is  650 Rs</br> ")
+// let finalv=650
+// document.write(" <p> your odered "+pant+"   item and your total is  "+ finalv*pant +'</p>')
+
+
+
+
+
+
+// document.write("<p> price of "+ item1 +"  is  1000 Rs "+'</p>')
+// let final=1000
+// document.write("  <p>your odered "+tShirt+"   item and your total is  "+ final*tShirt +'</p>')
+
+
+// question8
+
+
+// let num1 =+prompt('physics number')
+// let num3 =+prompt('chemistry number')
+// let num4 =+prompt('math number')
+// let num5 =+prompt('islamiat number')
+
+
+
+// let obtained = num1+num3+num4+ num5
+
+
+// let total=400
+
+
+// document.write('<p>your total  mark is 400 </p>' )
+// document.write('<p> your obtained  mark is  '+obtained+"</p>" )
+// let final= obtained*100/total
+// let change=final.toFixed(2)
+// document.write(" <p>your percentage is "+change+"%  "+'</p>')
+
+
+
+// question 11 age calculator 
+
+
+
+// var born =+prompt('which year do your born')
+// var current =2024
+// var final= current-born
+
+// document.write('your age is '+ final    )
+
+// question 13    (finish chapter 5)
+
+
+
+// var papar = prompt('whats  your favourite snack')
+// var age = +prompt('whats  your age')
+// var mage = +prompt('whats  your age')
+// var amount = +prompt('amount of snacks per day')
+// let total =(mage-age)*amount
+
+
+// document.write("<p> your favourite snack is"+ papar +"</p>")
+// document.write("<p> your age is "+ age+"</p>")
+// document.write("<p> your maximum age is"+mage +"</p>")
+// document.write("<p> amount os snack per day" +amount+"</p>")
+// document.write("<p>you will need  "+  total+  papar +"   to last you until the ripe old age of "+mage+"</p>")
